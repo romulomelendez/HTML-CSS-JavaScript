ef9fe9cf3ce9f7a9cc649beb04ad22550b8ddc29
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2021-04-30 07:59:25 -0300

    Implementing slide show

--- just_practing/estudo11/script.js
+++ just_practing/estudo11/script.js
@@ -1,23 +1,35 @@
 var images = []
+var loop
 
 const preloadImg = () => {
 
-    for ( let i = 0; i < 5; i++ ) {
+    let x = 1                         //    This variable stores a number that corresponds to the name of each image.
 
-        let x                           //    This variable stores a number that corresponds to the name of each image.      
-        img[i] = new Image()
-        img[i].src = 'images/img0' + x + '.jpg'
+    for ( let i = 0; i < 5; i++ ) {
+        
+        images[i] = new Image()
+        images[i].src = 'images/img0' + x + '.jpg'
         x++
+        
+    }
+   
+}
+
+const loadImg = () => {
+    
+    if ( images.length === 0 )  {
+
+        preloadImg()
 
     }
+    window.document.querySelector('div#containerImages').style.backgroundImage = "url('" + images[Math.floor(Math.random() * 5)].src + "')"
+    //window.document.querySelector('div#containerImages').style.backgroundImage = "url('" + images[changePhoto()].src + "')"
+    changePhoto()
 
 }
 
-const loadImg = (img) => {
-    
-    preloadImg()
-    window.document.querySelector('div#containerImages').style.backgroundImage = "url('" + imgs[img].src + "')"
+const changePhoto = () => loop = setInterval(loadImg, 10000)
+const stopSlider = () => clearInterval(loop)
+const stopClock = () => alert('Fui clicado!')
 
-}
-
-window.addEventListener('load', loadImg())
+window.addEventListener('load', loadImg)
