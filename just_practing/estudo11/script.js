b8d7b993ccdfa2cd9d6941459dbab9a678baa655
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2021-04-30 09:38:41 -0300

    Finish Slider

--- just_practing/estudo11/script.js
+++ just_practing/estudo11/script.js
@@ -1,5 +1,5 @@
 var images = []
-var loop
+
 
 const preloadImg = () => {
 
@@ -22,14 +22,25 @@ const loadImg = () => {
         preloadImg()
 
     }
+
     window.document.querySelector('div#containerImages').style.backgroundImage = "url('" + images[Math.floor(Math.random() * 5)].src + "')"
-    //window.document.querySelector('div#containerImages').style.backgroundImage = "url('" + images[changePhoto()].src + "')"
-    changePhoto()
 
 }
 
-const changePhoto = () => loop = setInterval(loadImg, 10000)
-const stopSlider = () => clearInterval(loop)
-const stopClock = () => alert('Fui clicado!')
+var loop = setInterval(loadImg, 1000)
 
-window.addEventListener('load', loadImg)
+const playSlider = () => {
+
+    loop = setInterval(loadImg, 1000)
+    window.document.getElementById('btnSlider').setAttribute("onclick", "stopSlider()")
+
+}
+
+const stopSlider = () => {
+    
+    clearInterval(loop)
+    window.document.getElementById('btnSlider').setAttribute("onclick", "playSlider()")
+
+}
+
+const stopClock = () => alert('Ainda vou implementar rs, CALMA!')
